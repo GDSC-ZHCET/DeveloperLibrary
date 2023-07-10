cf2513b50f5f8c4cbf2e729f5a5660b9faaf5a20
Sumir Vats <sumirvats@gmail.com>
2023-07-10 10:39:39 +0530

    Exapmle commit 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <h1>Welcome to dev library</h1>;
+  return (
+    <div>
+      <h1>Welcome to dev library</h1>
+      <p>Helllo</p>
+    </div>
+  );
 }
 
 export default App;

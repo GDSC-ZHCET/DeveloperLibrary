c2ed63925be761bcefe4d407396e263fbd2a4edd
Fiza Khan <fiza1892003@gmail.com>
2023-07-11 23:38:41 +0530

    added header and navbar

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+            <Navbar/>
+        </div>
+
+
+    );
+}
+
+export default Header;

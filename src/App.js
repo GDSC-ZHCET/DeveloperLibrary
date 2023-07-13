e941c1b96dff41d0daf0a3768730a0937c6f3923
Ujjwal <ujjwalvarshney1111@gmail.com>
2023-07-13 14:19:40 +0530

    Added discription.js and discription.css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Footer from './Footer';
 import './footer.css';
+import Discription from './Discription';
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <Discription/>
        <Footer/>
       </div>
     );
